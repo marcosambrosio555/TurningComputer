9364ccde68c8af79fdaf19d017bcb2b7be4f1553
Marcos Ambrósio <marcosambrosio555@gmail.com>
2024-04-23 11:00:55 +0100

    footer

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ btnMenu.addEventListener("click", () => {
     btnMenu.classList.toggle("active");
     menu.classList.toggle("show")
 });
+
+menu.querySelectorAll("a").forEach(a => {
+    a.addEventListener("click", () => {
+        btnMenu.classList.remove("active");
+        menu.classList.remove("show")
+    })
+})
