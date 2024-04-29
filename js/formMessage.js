76553fc89f80ee03e0f0fbe83eb8e23f7291f396
Marcos Ambrósio <marcosambrosio555@gmail.com>
2024-04-29 04:47:36 +0100

    Alteração com fotos

---
+++ js/formMessage.js
@@ -0,0 +1,19 @@
+const btnSendMessage = document.querySelector(
+    ".section-contact .card-form .btn"
+);
+
+const scriptURL =
+    "https://script.google.com/macros/s/AKfycbzyQtN-v2UihzR6lGLpn9hu6G4d8EPzOF7Bhh_FT4fI8VWsfvfkX_z9GRlN25P9qDQn/exec";
+const form = document.forms["contact-us"];
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    btnSendMessage.classList.add("loading");
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        .then((response) => {
+            btnSendMessage.classList.remove("loading");
+            form.reset();
+            console.log("Success!", response);
+        })
+        .catch((error) => console.error("Error!", error.message));
+});
